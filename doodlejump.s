030f530c354b84b9926cdd929142ceb28339fff0
Tony Cao <tony.cao1@outlook.com>
2020-11-24 23:27:56 -0500

    Drew 3 platforms statically

--- doodlejump.s
+++ doodlejump.s
@@ -1,54 +1,54 @@
 # Bitmap Display Configuration:
-# - Unit width in pixels: 8					     
-# - Unit height in pixels: 8
-# - Display width in pixels: 256
-# - Display height in pixels: 256
+# - Unit width in pixels: 16					     
+# - Unit height in pixels: 16
+# - Display width in pixels: 512
+# - Display height in pixels: 512
 # - Base Address for Display: 0x10008000 ($gp)
 #
 .data
 	displayAddress:	.word	0x10008000
-	screenWidth: 	.word 64
-	screenHeight: 	.word 64
+	screenWidth: 	.word 32
+	screenHeight: 	.word 32
+	platformWidth: .word  8
+	screenSize: .word 1024
 .text
 	lw $t0, displayAddress	# $t0 stores the base address for display
 	li $t1, 0xdeb887	# $t1 stores the brown
-	li $t2, 0xf5f5dc	# $t3 stores the beige colour
+	li $t2, 0xf5f5dc	# $t2 stores the beige colour
+	lw $t8, platformWidth
+	lw $t9, screenSize
 	
 	
 # Fill screen with background colour
 		
 loopinit: 
-	li $t4, 0 # Counter
-	li $t5, 1024
+	li $t3, 0 # Counter
 
-loop:
+backgroundloop:
 	sw $t2, 0($t0)
 	add $t0, $t0, 4 # Move along the screen
-	addi $t4, $t4, 1
-	bne $t4, $t5, loop
-	# Try to paint the whole screen green
-	
-platform:
+	addi $t3, $t3, 1
+	bne $t3, $t9, backgroundloop
+
+# Attempt to draw 3 platforms onto the background
+		
+init:
 	lw $t0, displayAddress
-	sw $t1, 48($t0)
-	sw $t1, 52($t0)
-	sw $t1, 56($t0)
-	sw $t1, 60($t0)
-	sw $t1, 64($t0)
-	sw $t1, 68($t0)
-	sw $t1, 72($t0)
-	sw $t1, 76($t0)
-	sw $t1, 80($t0)
+	addi $t3, $t0, 1496
+	addi $t4, $t0, 1968
+	addi $t5, $t0, 2304
+	li $t6, 0
 	
-	sw $t1, 304($t0)
-	sw $t1, 308($t0)
-	sw $t1, 312($t0)
-	sw $t1, 316($t0)
-	sw $t1, 320($t0)
-	sw $t1, 324($t0)
-	sw $t1, 328($t0)
-	sw $t1, 332($t0)
-	sw $t1, 336($t0)
+platformloop:
+	sw $t1, 0($t3)
+	sw $t1, 0($t4)
+	sw $t1, 0($t5)
+	addi $t3, $t3, 4
+	addi $t4, $t4, 4
+	addi $t5, $t5, 4
+	addi $t6, $t6, 1
+	bne $t6, $t8, platformloop
+	
 
 Exit:
 	li $v0, 10 # terminate the program gracefully
