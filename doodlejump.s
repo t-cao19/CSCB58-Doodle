54dfd495a000454f0679fc71c749edd36d8b9c00
Tony Cao <tony.cao1@outlook.com>
2020-11-24 23:47:10 -0500

    Drew static doodle jumper

--- doodlejump.s
+++ doodlejump.s
@@ -37,7 +37,9 @@
 	screenHeight: 	.word 32
 	platformWidth: .word  8
 	screenSize: .word 1024
+	
 .text
+main:
 	lw $t0, displayAddress	# $t0 stores the base address for display
 	li $t1, 0xdeb887	# $t1 stores the brown
 	li $t2, 0xf5f5dc	# $t2 stores the beige colour
@@ -47,7 +49,7 @@
 	
 # Fill screen with background colour
 		
-loopinit: 
+backgroundinit: 
 	li $t3, 0 # Counter
 
 backgroundloop:
@@ -58,11 +60,11 @@ backgroundloop:
 
 # Attempt to draw 3 platforms onto the background
 		
-init:
+platforminit:
 	lw $t0, displayAddress
-	addi $t3, $t0, 1496
-	addi $t4, $t0, 1968
-	addi $t5, $t0, 2304
+	addi $t3, $t0, 2264
+	addi $t4, $t0, 2736
+	addi $t5, $t0, 3072
 	li $t6, 0
 	
 platformloop:
@@ -75,6 +77,17 @@ platformloop:
 	addi $t6, $t6, 1
 	bne $t6, $t8, platformloop
 	
+# Attempt to draw the doodle
+
+doodledraw:
+	li $t3, 0x556b2f	# $t3 stores colour of doodle
+	#addi $t4, $t0, 4028
+	sw $t3, 3776($t0)
+	sw $t3, 3900($t0)
+	sw $t3, 3904($t0)
+	sw $t3, 3908($t0)
+	sw $t3, 4028($t0)
+	sw $t3, 4036($t0)
 
 Exit:
 	li $v0, 10 # terminate the program gracefully
