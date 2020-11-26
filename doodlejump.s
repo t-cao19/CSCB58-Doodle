b43d5104e7e1d66c6fb04c9af9572653287b26aa
Tony Cao <tony.cao1@outlook.com>
2020-11-25 23:30:46 -0500

    Stored platform coordinates in an array and generated 5 random platforms

--- doodlejump.s
+++ doodlejump.s
@@ -35,20 +35,20 @@
 	displayAddress:	.word	0x10008000
 	screenWidth: 	.word 32
 	screenHeight: 	.word 32
-	platformWidth: .word  8
+	platformWidth: .word  6
 	screenSize: .word 1024
+	platforms: .space 20
 	
 .text
 main:
 	lw $t0, displayAddress	# $t0 stores the base address for display
 	li $t1, 0xdeb887	# $t1 stores the brown
 	li $t2, 0xf5f5dc	# $t2 stores the beige colour
-	lw $t8, platformWidth
+	la $t8, platforms 	# Array with 6 int spots for platform locations
 	lw $t9, screenSize
 	
 	
 # Fill screen with background colour
-		
 backgroundinit: 
 	li $t3, 0 # Counter
 
@@ -60,22 +60,54 @@ backgroundloop:
 
 # Attempt to draw 3 platforms onto the background
 		
-platforminit:
+generateplatform:
 	lw $t0, displayAddress
-	addi $t3, $t0, 2264
-	addi $t4, $t0, 2736
-	addi $t5, $t0, 3072
+	addi $t4, $t4, 0 # Counter
+	
+randomplatform:
+	add $t5, $t8, $t4
+
+	# Generate a random platform coordinate
+	li $v0, 42
+	li $a0, 0
+	li $a1, 1024
+	syscall
+	
+	# Store the randomly generated coordinate
+	li $t6, 4
+	mult $a0, $t6
+	mflo $t7
+	add $t3, $t0, $t7
+	sw $t3, 0($t5)
+	addi $t4, $t4, 4
+	bne $t4, 20, randomplatform
+	
+	#add $t3, $t0, $a0
+	#addi $t3, $t0, 2264
+	#sw $t3, 0($t7)
+	#addi $t3, $t0, 2736
+	#sw $t3, 4($t7)
+	#addi $t3, $t0, 3072
+	#sw $t3, 8($t7)
+	#addi $t3, $t0, 1816
+	#sw $t3, 12($t7)
+	#addi $t3, $t0, 1476
+	#sw $t3, 16($t7)
+	
 	li $t6, 0
 	
 platformloop:
-	sw $t1, 0($t3)
-	sw $t1, 0($t4)
-	sw $t1, 0($t5)
-	addi $t3, $t3, 4
-	addi $t4, $t4, 4
-	addi $t5, $t5, 4
-	addi $t6, $t6, 1
-	bne $t6, $t8, platformloop
+	add $t3, $t8, $t6 # Offset/index variable i
+	lw $s4, 0($t3)    # Load the element from that position# Get to that position of the screen
+	sw $t1, 0($s4)
+	sw $t1, 4($s4)
+	sw $t1, 8($s4)
+	sw $t1, 12($s4)
+	sw $t1, 16($s4)
+	sw $t1, 20($s4)
+	sw $t1, 24($s4)
+	addi $t6, $t6, 4
+	bne $t6, 20, platformloop
 	
 # Attempt to draw the doodle
 
